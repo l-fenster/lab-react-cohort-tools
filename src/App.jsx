7f575dd70630f0bcce3e2b0c7133f05049a583f2
Leah Fenster <leahefen@gmail.com>
2024-08-03 11:12:30 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
-
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
+      <Routes className="pages">
+        <Route path="/" element={<HomePage />} />
 
-      <div className="pages">
-        <HomePage />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} />
 
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+/*
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/students" element={<Students students={data} />} />
+        <Route
+          path="/students/:studentId"
+          element={<SingleStudent students={data} />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+    */
